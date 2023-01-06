d69ed1d1f62f94dfb76f1512959c6b9463f49c05
LightmanBro <omotosodavidromeo@gmail.com>
2023-01-06 10:34:45 +0100

    learning and Practicing the data types

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("this is not goig to have a new line");
+        string school = "BYU Idaho";
+        Console.WriteLine($"I am studying at {school}.");
     }
 }
