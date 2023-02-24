9ed61ea69824d62db454729e140a15b8112a3e0e
LightmanBro <omotosodavidromeo@gmail.com>
2023-02-24 18:24:06 +0100

    completing the listening class

--- prove/Develop04/Listening.cs
+++ prove/Develop04/Listening.cs
@@ -4,21 +4,22 @@ class Listening:Activity{
     private List<string> _listeningQues;
     private List<string> _listeningAns;
     
-    private void displayQues(){
-        //loop through all the questions and display it randomly one after the other to the console.
+    private string displayQues(){
+        //loop through all the questions and display it randomly one after and return it each so as to save it inside a variable.
+
+        return //question
     }
 
+
+    //take the returned question and pass it as a parameter to this function
+    public void saveAns(string ques, string ans){
+		_listeningAns.Add($"{ques}\n{ans}");
+        }
     public void listen(){
         //welcoming message
         displayMessage();
-        //display the questions inside the list
-        displayQues();
         //start the countdown timer
         countdownTimer();
-        //display the spinner
-        displaySpinner();
-
-
         // time class function
         DateTime startTime = DateTime.Now;
         DateTime futureTime = startTime.AddSeconds(5000);
@@ -27,6 +28,16 @@ class Listening:Activity{
         //condition to run the program with time
         if (currentTime < futureTime)
         {
+            //display the questions inside the list
+            string ques = displayQues();
+            
+            //display the spinner awaiting an answer to the question
+            displaySpinner();
+
+            Console.Write(">");
+            string ans = Console.ReadLine();
+            //save the answers inside a new list so it could be displayed later
+            saveAns(ques,ans);
 
             // perform all the activities here
             Console.WriteLine("We have not arrived at our future time yet...");
