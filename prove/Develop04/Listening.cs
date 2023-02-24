5d2456f630ea73523d4de320a55797e0eb0a96b2
LightmanBro <omotosodavidromeo@gmail.com>
2023-02-24 18:06:09 +0100

    ....

---
+++ prove/Develop04/Listening.cs
@@ -0,0 +1,11 @@
+using System;
+class Listening:Activity{
+
+    private List<string> _listeningQues;
+    private List<string> _listeningAns;
+    
+    private void displayQues(){
+        //loop through all the questions and display it randomly one after the other to the console.
+    }
+
+}
