12fb9cc69150954f628301dd087d0bc341572896
LightmanBro <omotosodavidromeo@gmail.com>
2023-02-24 18:11:26 +0100

    started the structure of the listening class

--- prove/Develop04/Listening.cs
+++ prove/Develop04/Listening.cs
@@ -8,4 +8,31 @@ class Listening:Activity{
         //loop through all the questions and display it randomly one after the other to the console.
     }
 
+    public void listen(){
+        //welcoming message
+        displayMessage();
+        //display the questions inside the list
+        displayQues();
+        //start the countdown timer
+        countdownTimer();
+        //display the spinner
+        displaySpinner();
+
+
+        // time class function
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(5000);
+        DateTime currentTime = DateTime.Now;
+
+        //condition to run the program with time
+        if (currentTime < futureTime)
+        {
+
+            // perform all the activities here
+            Console.WriteLine("We have not arrived at our future time yet...");
+        }else if(currentTime == futureTime){
+
+            finishingMsg();
+        }
+    }
 }
