050d3f80406781cda379de1e0d16d0e1077f2ce9
LightmanBro <omotosodavidromeo@gmail.com>
2023-03-28 14:26:53 +0100

    Started working on the Online Ordering Program

---
+++ final/Foundation2/Products.cs
@@ -0,0 +1,12 @@
+public class Products{
+
+    private string _productName;
+    private string _productId;
+    private int _productQty;
+
+    public Products(string name, string id, int qty){
+        _productName = name;
+        _productId = id;
+        _productQty = qty;
+    }
+}
