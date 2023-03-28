f1fcab75afbeda9d402a7248380d3de581d2371e
LightmanBro <omotosodavidromeo@gmail.com>
2023-03-28 13:36:42 +0100

    Added commentś to the program

--- final/Foundation1/Comments.cs
+++ final/Foundation1/Comments.cs
@@ -3,6 +3,8 @@ public class Comments{
     public string _commenterName;
     public string _comments;
 
+
+    //Created a setter function on the comment class.
     public string addComments(string name,string com){
        
         return $"{name} comment: {com}";
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -5,7 +5,11 @@ public class Video
     public string _author;
     public double _length;
 
+
+    //Created a list to add the comments on each video
     List<string> comments = new List<string>();
+
+    //Created a constructor function to set the member variables.
     public Video(string title, string author, double length)
     {
         _title = title;
@@ -13,10 +17,13 @@ public class Video
         _length = length;
     }
 
+    //Setter method to populate the empty comment list.
     public void addComentsToList(string c)
     {
         comments.Add(c);
     }
+
+    //Getter method to display the properties of the videos.
     public void displayVideo()
     {
         Console.WriteLine();
@@ -25,6 +32,8 @@ public class Video
         Console.WriteLine($"Posted by: {_author}\n___________________ ");
         Console.WriteLine($" {comments.Count()} comments");
         Console.WriteLine();
+
+        //Loop through all the elemets inside the list and print them out in the console.
         foreach (var com in comments)
         {
             Console.WriteLine(com);
