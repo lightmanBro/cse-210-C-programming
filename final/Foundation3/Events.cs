d004524f8842bf78770d8a20e19f343044b0d1aa
LightmanBro <omotosodavidromeo@gmail.com>
2023-03-29 17:02:43 +0100

    Started the Event Planning program

---
+++ final/Foundation3/Events.cs
@@ -0,0 +1,11 @@
+public class Event{
+    private string _eventTitle;
+    private string _eventDesc;
+    private string _eventDate;
+    private string _eventAddress;
+
+
+    public void standardDetails(){
+        
+    }
+}
